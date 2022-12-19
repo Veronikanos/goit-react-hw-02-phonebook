986a30c529cd6bb806ae85371b7ebf9c9c33aba3
Veronica <vveronikanika@gmail.com>
2022-12-19 18:51:07 +0200

    feat: create form and input, show results

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  addContact = text => {
+    const newObj = {
+      id: nanoid(),
+      name: text,
+    };
+
+    this.setState(prevState => ({
+      contacts: [newObj, ...prevState.contacts],
+    }));
+
+    console.log(this.state);
+  };
+
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.addContact(this.state.name);
+
+    this.setState({ name: '' }); //reset input
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <h1>Phonebook</h1>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor={this.nameInputId}>
+              <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                value={this.state.name}
+                onChange={this.handleInput}
+                id={nanoid()}
+              />
+              <button type="submit">Add Contact</button>
+            </label>
+          </form>
+        </div>
+        <div>
+          <h2>Contacts</h2>
+          <ul>
+            {this.state.contacts.map(item => (
+              <li key={nanoid()}>{item.name}</li>
+            ))}
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
