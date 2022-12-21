2b6219a4d947f271ab47243f5a48d4ca36eaef62
Veronica <vveronikanika@gmail.com>
2022-12-21 16:01:48 +0200

    feat: add filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,15 @@ import { Contacts } from './Contacts/Contacts';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
-    phone: '',
+    number: '',
   };
 
   // addContact = text => {
@@ -34,17 +40,27 @@ export class App extends Component {
     const newObj = {
       id: nanoid(),
       name: this.state.name,
-      phone: this.state.phone,
+      number: this.state.number,
     };
 
     this.setState(prevState => ({
       contacts: [newObj, ...prevState.contacts],
     }));
 
-    this.setState({ name: '', phone: '' }); //reset input
+    this.setState({ name: '', number: '' }); //reset input
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
   };
 
   render() {
+    const normalizeText = this.state.filter.toLowerCase();
+
+    const filteredContacts = this.state.contacts.filter(item =>
+      item.name.toLowerCase().includes(normalizeText)
+    );
+
     return (
       <section className={styles.phonebook}>
         <div className={styles.container}>
@@ -62,7 +78,7 @@ export class App extends Component {
                 required
                 value={this.state.name}
                 onChange={this.handleInput}
-                // id={nanoid()} ?????
+                id={nanoid()}
               />
               {/* <button type="submit">Add Contact</button> */}
             </label>
@@ -71,23 +87,28 @@ export class App extends Component {
             <label>
               <input
                 className={styles.inputField}
-                placeholder="Phone"
-                type="phone"
-                name="phone"
-                // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                placeholder="Phone number"
+                type="tel"
+                name="number"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                 required
-                value={this.state.phone}
+                value={this.state.number}
                 onChange={this.handleInput}
-                // id={nanoid()} ??????
+                id={nanoid()}
               />
             </label>
+
             <button type="submit" className={styles.button}>
               Add Contact
             </button>
           </form>
         </div>
-        <Contacts contacts={this.state.contacts} />
+        <Contacts
+          contacts={filteredContacts}
+          filter={this.state.filter}
+          onChange={this.changeFilter}
+        />
       </section>
     );
   }
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,15 +1,21 @@
 import styles from './Contacts.module.css';
-import React, { Component } from 'react';
+import React from 'react';
 import { nanoid } from 'nanoid';
 
-export const Contacts = ({ contacts }) => {
+export const Contacts = ({ contacts, filter, onChange }) => {
   return (
     <div className={styles.container}>
       <h2>Contacts</h2>
+
+      <label htmlFor="">
+        <p>Find contacts by name</p>
+        <input type="text" value={filter} onChange={onChange} />
+      </label>
+
       <ol>
         {contacts.map(item => (
           <li key={nanoid()}>
-            {item.name} {item.phone}
+            {item.name} {item.number}
           </li>
         ))}
       </ol>
